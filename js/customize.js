7cf0045de582caea8fc51e8c3984a2675957bb07
Miley <miley.ho@hyweb.com.tw>
2021-11-10 16:35:38 +0800

    update

--- js/customize.js
+++ js/customize.js
@@ -220,18 +220,18 @@ $(function() {
 
     });
 
-    function deIdentification(str) {
-        const showLen = Math.round(str.length / 2); // 顯示幾個
-        const markLen = str.length - showLen; // 要隱藏幾個
-        const showStart = Math.round((str.length - showLen) / 2); // 從哪開始隱
-        return str.replace(str.substr(showStart, markLen), '*'.repeat(markLen));
-    }
-    var mask_text = $(".mask_text")
-    mask_text.each(function() {
-        var new_text = $(this).text()
-        var inntext = deIdentification(new_text)
-        $(this).text(inntext)
-    })
+    // function deIdentification(str) {
+    //     const showLen = Math.round(str.length / 2); // 顯示幾個
+    //     const markLen = str.length - showLen; // 要隱藏幾個
+    //     const showStart = Math.round((str.length - showLen) / 2); // 從哪開始隱
+    //     return str.replace(str.substr(showStart, markLen), '*'.repeat(markLen));
+    // }
+    // var mask_text = $(".mask_text")
+    // mask_text.each(function() {
+    //     var new_text = $(this).text()
+    //     var inntext = deIdentification(new_text)
+    //     $(this).text(inntext)
+    // })
 
 
 
